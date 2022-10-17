0c9f7e3b31a1694346701e4ece4dd746e8a39db7
Weydson Marinho <weydsonmarinho@gmail.com>
2022-10-17 11:31:44 -0300

    Dashboard Screen: Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/49030804?v=4' }} />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Fulano</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
