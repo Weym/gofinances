9f6f68b4e5b2d5ca0f49f5d83e38338baa94bdf5
Weydson Marinho <weydsonmarinho@gmail.com>
2022-10-17 13:51:27 -0300

    Displaying TransactionCard content dynamically

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -23,6 +23,8 @@ import {
 export function Dashboard() {
   const data = [
     {
+      id: '1',
+      type: 'positive',
       title: 'Desenvolvimento de site',
       amount: 'R$ 12.000,00',
       category: {
@@ -32,22 +34,26 @@ export function Dashboard() {
       date: '13/04/2020',
     },
     {
-      title: 'Desenvolvimento de site',
-      amount: 'R$ 12.000,00',
+      id: '2',
+      type: 'negative',
+      title: 'Hamburgueria Pizzy',
+      amount: 'R$ 59,00',
       category: {
-        name: 'Vendas',
-        icon: 'dollar-sign',
+        name: 'Alimentação',
+        icon: 'coffee',
       },
-      date: '13/04/2020',
+      date: '10/04/2020',
     },
     {
-      title: 'Desenvolvimento de site',
-      amount: 'R$ 12.000,00',
+      id: '3',
+      type: 'negative',
+      title: 'Aluguel do apartamento',
+      amount: 'R$ 1.200,00',
       category: {
-        name: 'Vendas',
-        icon: 'dollar-sign',
+        name: 'Casa',
+        icon: 'shopping-bag',
       },
-      date: '13/04/2020',
+      date: '27/03/2020',
     },
   ];
 
