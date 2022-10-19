c5e1d5326a092fc9fa556dbd61821d9b8599ad5d
Weydson Marinho <weydsonmarinho@gmail.com>
2022-10-19 18:47:55 -0300

    Resume Screen: Working on generating an graph to display expenses

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,8 +1,9 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Platform } from 'react-native';
+import { useTheme } from 'styled-components';
 import { MaterialIcons } from '@expo/vector-icons';
 
-import { useTheme } from 'styled-components';
+import { Resume } from '../screens/Resume';
 import { Register } from '../screens/Register';
 import { Dashboard } from '../screens/Dashboard';
 
@@ -43,7 +44,7 @@ export function AppRoutes() {
       />
       <Screen
         name="Resumo"
-        component={Register}
+        component={Resume}
         options={{
           tabBarIcon: ({ size, color }) => (
             <MaterialIcons name="pie-chart" size={size} color={color} />
---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { HistoryCard } from '../../components/HistoryCard';
+
+import { Container, Header, Title, Content } from './styles';
+import { categories } from '../../utils/categories';
+
+interface TransactionData {
+  type: 'positive' | 'negative';
+  name: string;
+  amount: string;
+  category: string;
+  date: string;
+}
+
+interface CategoryData {
+  key: string;
+  name: string;
+  total: number;
+  totalFormatted: string;
+  color: string;
+}
+
+export function Resume() {
+  const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([]);
+
+  async function loadData() {
+    const dataKey = '@gofinance:transactions';
+    const response = await AsyncStorage.getItem(dataKey);
+    const responseFormatted = response ? JSON.parse(response) : [];
+
+    const expenses = responseFormatted.filter(
+      (expense: TransactionData) => expense.type === 'negative'
+    );
+
+    const totalByCategory: CategoryData[] = [];
+
+    categories.forEach((category) => {
+      let categorySum = 0;
+
+      expenses.forEach((expense: TransactionData) => {
+        if (expense.category === category.key) {
+          categorySum += Number(expense.amount);
+        }
+      });
+
+      if (categorySum > 0) {
+        const totalFormatted = categorySum.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        });
+
+        totalByCategory.push({
+          key: category.key,
+          name: category.name,
+          color: category.color,
+          total: categorySum,
+          totalFormatted,
+        });
+      }
+    });
+
+    setTotalByCategories(totalByCategory);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <Title>Resumo por categoria</Title>
+      </Header>
+      <Content>
+        {totalByCategories.map((item) => (
+          <HistoryCard
+            key={item.key}
+            title={item.name}
+            amount={item.totalFormatted}
+            color={item.color}
+          />
+        ))}
+      </Content>
+    </Container>
+  );
+}
